9038da5217733137f840bc7c917a561625805cc0
jerry <secret78510@gmail.com>
2023-12-28 19:34:19 +0800

    no message

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
  
+
+//直接作用
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
